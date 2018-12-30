d6bbbaec97aef1eca1d1e6dde7158999625791ae
Volodymyr Kryviak <vkryviak@twilio.com>
2018-12-30 15:02:01 +0200

    task-0A Use object instead of var

--- src/client/App.js
+++ src/client/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = {
-    username: null,
-    name:'Google',
-    url:'http://google.com.ua',
-  };
+    state = {
+        username: null,
+        link:
+            {
+                name: 'Google',
+                url: 'http://google.com.ua',
+            },
+    };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+    componentDidMount() {
+        fetch('/api/getUsername')
+            .then(res => res.json())
+            .then(user => this.setState({username: user.username}));
+    }
 
-  render() {
-    const {username,name,url} = this.state;
-    return (
-      <div>
-        {username ? <div><h1>Hello {username}</h1>  <p>Hello</p></div> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-        <p className="main-link"> - <a href={url}>{name}</a></p>
-      </div>
-    );
-  }
+    render() {
+        const {username, link} = this.state;
+        return (
+            <div>
+                {username ? <div><h1>Hello {username}</h1>  <p>Hello</p></div> : <h1>Loading.. please wait!</h1>}
+                <img src={ReactImage} alt="react"/>
+                <p className="main-link"> - <a href={link.url}>{link.name}</a></p>
+            </div>
+        );
+    }
 }
