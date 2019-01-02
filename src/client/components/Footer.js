ed82b140bd0b20c8806fa702d60b985d4a59a3f7
Bohdan <boyavor@gmail.com>
2019-01-02 19:15:13 +0200

    task iV2vejQJ/6-add-footer-component

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-export default class App extends Component {
+export default class Footer extends Component {
   render() {
     return (
       <footer>
