2e33fb885f317f9c7ecea3d3eac8d15467c13fde
Bohdan <boyavor@gmail.com>
2019-01-04 18:18:23 +0200

    task-WmPPecgQ added image in new folder creted new ccs-file Puted image like a background for Header.

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./header.css";
+import We_The_Best_logo from '../img/We_The_Best_logo.png';
 
 export default class Header extends Component {
     render() {
