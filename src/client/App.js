0f01413241a14a16cde0f85812cc1c8dfc220614
Volodymyr Kryviak <vkryviak@twilio.com>
2019-01-04 22:49:39 +0200

    task-FkRtfqBM Revert "Delete App.js"

---
+++ src/client/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './app.css';
+
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Header from './components/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Menu/>
+        <Main/>
+        <Header/>
+        <Footer/>
+      </React.Fragment>
+    );
+  }
+}
