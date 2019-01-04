16e41d7f4c199e65b6ac41c0e21de97bbd187f0b
Bohdan <boyavor@gmail.com>
2019-01-04 21:33:40 +0200

    task-WmPPecgQ added image

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import "./header.css";
 import We_The_Best_logo from '../img/We_The_Best_logo.png';
-import ReactImage from '../react.png';
 
 export default class Header extends Component {
     render() {
