e4368e016c5a23a1e0865a97c0ff98321601183e
Shaleniy <ofirych@gmail.com>
2019-01-05 22:58:21 +0200

    task-8gCjnepo remane classNames

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -12,9 +12,9 @@ export default class Main extends Component {
     render() {
         const { username } = this.state;
         return (
-          <main className = "mainText">
+          <main className = "main-text">
                 {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-                <img className="mainImg" src={ReactImage} alt="react" />
+                <img className="main-img" src={ReactImage} alt="react" />
           </main>
 
         );
