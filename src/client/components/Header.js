ec159bdd8e8692d217723c64737167423b9ad992
Bohdan <boyavor@gmail.com>
2019-01-08 16:25:42 +0200

    task-WmPPecgQ Created assets folder in client folder Puted image into this folder Change background Added color - green

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './header.css';
+import We_The_Best_logo from '../image/We_The_Best_logo.png';
 
 export default class Header extends Component {
-    render() {
-        return (
-            <header className = "main-greeting"> Welcome to the Best Project </header>
-        );
-    }
+  render() {
+    return (
+      <header className="main-greeting"> Welcome to the Best Project <img className="header-img"
+                                                                          src={We_The_Best_logo}
+                                                                          alt="We_The_Best_logo"/>
+      </header>
+    );
+  }
 }
