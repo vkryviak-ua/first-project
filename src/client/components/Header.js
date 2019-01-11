56f503f0f6eae995d143c8938e83318fe07fa9b1
Shaleniy <ofirych@gmail.com>
2019-01-11 17:24:07 +0200

    task-8e6bDM2l created new folder assets and put simple image, add background image for Header,  add background color orange

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './header.css';
+import Link256 from '../assets/link-256.png'
 
 export default class Header extends Component {
     render() {
         return (
-            <header className="main-greeting"> Welcome to the Best Project </header>
+            <header className="main-greeting"> Welcome to the Best Project
+                <img className="header-img" src={Link256} alt="simpleImg"/>
+            </header>
         );
     }
 }
