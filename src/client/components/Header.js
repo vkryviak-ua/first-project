1ccf415ece0d7ef14552142c6dee10fa983414df
Bohdan <boyavor@gmail.com>
2019-01-14 14:59:27 +0200

    Change background for Header
    https://trello.com/c/WmPPecgQ/18-change-background-for-header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import './header.css';
-import We_The_Best_logo from '../image/We_The_Best_logo.png';
 
 export default class Header extends Component {
   render() {
     return (
-      <header className="main-greeting"> Welcome to the Best Project <img className="header-img"
-                                                                          src={We_The_Best_logo}
-                                                                          alt="We_The_Best_logo"/>
-      </header>
+      <header className="main-greeting"> Welcome to the Best Project </header>
     );
   }
 }
