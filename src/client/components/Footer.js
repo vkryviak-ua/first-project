ccd683ebc0a2f5df4a986d6077e3ef0eebfb885f
Shaleniy <ofirych@gmail.com>
2019-01-18 17:53:44 +0200

    task-900aW076-2 remove some stile

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -4,9 +4,9 @@ import './footer.css';
 export default class Footer extends Component {
   render() {
     return (
-        <footer className="global-footer">
+      <footer className="global-footer">
             ©2019 Best Team. All rights reserved.
-        </footer>
+      </footer>
     );
   }
 }
