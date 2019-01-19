e337cc4cd3f3f6184dee3fb5cf070feb0dff2ab3
Bodyk <boyavor@gmail.com>
2019-01-19 23:36:24 +0200

    task-ePphci4j  returned original whitespaces

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -3,28 +3,26 @@ import ReactImage from '../react.png';
 import './main.css';
 
 export default class Main extends Component {
-  state = { username: null };
+    state = { username: null };
   clickOnMe = () => {
     alert('You are awesome');
   };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
-  render() {
-    const { username } = this.state;
-    return (
-      <main className="global-main">
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img className="main-img" src={ReactImage} alt="react"/>
-
-        <button onClick={this.clickOnMe}>
-          Click on me
-        </button>
-      </main>
-    );
-  }
+    componentDidMount() {
+        fetch('/api/getUsername')
+          .then(res => res.json())
+          .then(user => this.setState({ username: user.username }));
+    }
+    render() {
+        const { username } = this.state;
+        return (
+          <main className="global-main">
+                {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+                <img className="main-img" src={ReactImage} alt="react" />
+            <button onClick={this.clickOnMe}>
+              Click on me
+            </button>
+          </main>
+        );
+    }
 }
