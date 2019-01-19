8862af600b182680bcc838c64e0e3ac5618a8d54
Bodyk <boyavor@gmail.com>
2019-01-19 18:36:09 +0200

    task-F9mdzg4P Added button and click Button red with black border. Added click event for it. Show alert with text "You are awesome"
    https://trello.com/c/F9mdzg4P/32-menu-add-button-and-click

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import ReactImage from '../react.png';
 import './main.css';
-export default class Main extends Component {
-    state = { username: null };
 
-    componentDidMount() {
-        fetch('/api/getUsername')
-          .then(res => res.json())
-          .then(user => this.setState({ username: user.username }));
-    }
-    render() {
-        const { username } = this.state;
-        return (
-          <main className="global-main">
-                {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-                <img className="main-img" src={ReactImage} alt="react" />
-          </main>
-        );
-    }
+export default class Main extends Component {
+  state = { username: null };
+  clickOnMe = () => {
+    alert('You are awesome');
+  };
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    const { username } = this.state;
+    return (
+      <main className="global-main">
+        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <img className="main-img" src={ReactImage} alt="react"/>
+
+        <button onClick={this.clickOnMe}>
+          Click on me
+        </button>
+      </main>
+    );
+  }
 }
