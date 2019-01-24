a8142bd3aab8ea7ffbb88deaa9f828f5f62d95c4
Shaleniy <ofirych@gmail.com>
2019-01-24 15:34:03 +0200

    task-wLxLFVi0 remove function View, remove tag h4 and aad tag div ror each tape

--- src/client/components/Info.js
+++ src/client/components/Info.js
@@ -3,20 +3,14 @@ import './info.css';
 
 export default class Info extends Component {
   render() {
-    function View() {
-    }
     return (
       <aside className="global-info">
         New component Info.
-      <div>
-        <h4>Name</h4>
-        <input></input>
-      </div>
-      <div>
-        <h4>Description</h4>
-        <textarea></textarea>
-        <button onClick={this.View}> View </button>
-      </div>
+      <div>Name</div>
+      <div><input></input></div>
+      <div>Description</div>
+      <div><textarea></textarea></div>
+      <div><button onClick={this.View}> View </button></div>
       </aside>
     );
   }
