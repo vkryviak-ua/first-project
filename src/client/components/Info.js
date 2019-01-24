260f1e429407d10bf88a61add5a00c7b672c1791
Shaleniy <ofirych@gmail.com>
2019-01-24 14:42:23 +0200

    task-wLxLFVi0 add form in Info component,Add input, textarea and button.

--- src/client/components/Info.js
+++ src/client/components/Info.js
@@ -3,9 +3,20 @@ import './info.css';
 
 export default class Info extends Component {
   render() {
+    function View() {
+    }
     return (
       <aside className="global-info">
         New component Info.
+      <div>
+        <h4>Name</h4>
+        <input></input>
+      </div>
+      <div>
+        <h4>Description</h4>
+        <textarea></textarea>
+        <button onClick={this.View}> View </button>
+      </div>
       </aside>
     );
   }
