6dfe09ffcff7aff99108d45d2143c51722379520
Shaleniy <ofirych@gmail.com>
2019-01-27 13:27:57 +0200

    task-EWbn3D6g add function pushMe, add click event for it, add className for button and color red with black border, add alert for this button with text "This is a form".

--- src/client/components/Info.js
+++ src/client/components/Info.js
@@ -3,14 +3,17 @@ import './info.css';
 
 export default class Info extends Component {
   render() {
+    function pushMe() {
+      alert("This is a form");
+    }
     return (
       <aside className="global-info">
         New component Info.
-        <div>Name</div>
-        <div><input/></div>
-        <div>Description</div>
-        <div><textarea></textarea></div>
-        <div><button> View </button></div>
+      <div>Name</div>
+      <div><input/></div>
+      <div>Description</div>
+      <div><textarea></textarea></div>
+      <div><button className="info-button" onClick={pushMe}> View </button></div>
       </aside>
     );
   }
