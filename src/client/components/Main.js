c1e4d9c2d21ecc7322eb958b71b06af80c1f6a55
Shaleniy <ofirych@gmail.com>
2019-02-19 16:58:54 +0200

    task-78KppKzl add click on image with text: "You have pressed on image" after image, add in state showTextAfterImage, Add onclick to image, add function pressTheImage

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactImage from '../react.png';
 import './main.css';
 import Article from './Article';
 
 export default class Main extends Component {
-    state = { username: null };
+	state = {
+		username: null,
+		showTextAfterImage: '',
+	};
 
-    componentDidMount() {
-        fetch('/api/getUsername')
-          .then(res => res.json())
-          .then(user => this.setState({ username: user.username }));
-    }
-    render() {
-        const { username } = this.state;
-        return (
-          <main className="global-main">
-            {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-            <img className="main-img" src={ReactImage} alt="react" />
-            <Article/>
-          </main>
-        );
-    }
+	pressTheImage = () => {
+		this.setState({showTextAfterImage: true});
+		return alert("You have pressed on image");
+	};
+
+	componentDidMount() {
+		fetch('/api/getUsername')
+			.then(res => res.json())
+			.then(user => this.setState({username: user.username}));
+	}
+
+	render() {
+		const {username} = this.state;
+		return (
+			<main className="global-main">
+				{username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+				<img className="main-img" src={ReactImage} alt="react" onClick={this.pressTheImage}/>
+				<Article/>
+			</main>
+		);
+	}
 }
