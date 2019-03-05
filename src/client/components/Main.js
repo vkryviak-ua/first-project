0da6c6cce7c5ddec32ca94302392a6d32c17b516
Bodyk <boyavor@gmail.com>
2019-03-05 16:25:36 +0200

    task-LtFDCXvu added css-property float right and padding 8px https://trello.com/c/LtFDCXvu/46-main-show-date-time c

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -30,8 +30,8 @@ export default class Main extends Component {
 		const { username, showTextAfterImage } = this.state;
 		return (
 			<main className="global-main">
-				{username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
 				<div className="dateAndTime"> {`${year}-${mount}-${day} ${hour}:${minutes}`} </div>
+				{username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
 				<img className="main-img" src={ReactImage} alt="react" onClick={this.pressTheImage}/>
 				{showTextAfterImage ? <div>You have pressed on image</div> : ''}
 				<Article/>
